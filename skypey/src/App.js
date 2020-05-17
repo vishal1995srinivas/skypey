15fbbfddc84bc62b59eeb2535615a7e50f5b3547
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-17 18:30:56 +0530

    init

---
+++ skypey/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SideBar from './SideBar';
+import Main from './Main';
+
+function App() {
+	return (
+		<div className="App">
+			<SideBar />
+			<Main />
+		</div>
+	);
+}
+
+export default App;
