1d6777daf8012dbf22a47cce816ad3e7c061a5ef
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-17 19:29:04 +0530

    init state

---
+++ skypey/src/Store/Istate.js
@@ -0,0 +1,53 @@
+state = {
+	user: {
+		name: 'VS',
+		email: 'vs@gmial.com',
+		profile_pic: 'fsdfdsf@df.com',
+		status: 'Author',
+		user_id: 'kdnsldn12ldfn3'
+	},
+	messages: {
+		asdasd34: {
+			0: {
+				is_user_msg: false,
+				number: 0,
+				text: 'Hello'
+			},
+			1: {
+				is_user_msg: true,
+				number: 1,
+				text: 'Hi'
+			}
+		},
+		sasdaads32: {
+			0: {
+				is_user_msg: false,
+				number: 0,
+				text: 'you know redux'
+			},
+			1: {
+				is_user_msg: true,
+				number: 1,
+				text: 'I do '
+			}
+		}
+	},
+	typing: '',
+	contacts: {
+		sdfdsftg56: {
+			name: 'josdjfo',
+			email: 'fake@gmail.com',
+			profile_pic: 'dfsdfsdf@gf.com',
+			status: 'fjsd jnjds jfnjdf',
+			user_id: 'sfsdfsdf'
+		},
+		SDFsdf45: {
+			name: 'Doyle',
+			email: 'dsfsdf@gmail.com',
+			profile_pic: 'sfsdfs@df.com',
+			status: 'dsfdsf sdfsd fsdf',
+			user_id: 'dsfsdf$54'
+		}
+	},
+	activeUserId: 'ssdfsdf3'
+};
