564f0bff8b5e6ea16a5c9215460eca2cdc2dd856
Vishal Srinivas <vishal1995srinivas@gmail.com>
2020-05-18 16:41:17 +0530

    revamp

--- skypey/src/Chats.js
+++ skypey/src/Chats.js
@@ -1,17 +1,17 @@
 import React, { Component } from 'react';
-import store from './Store';
 import './Chats.css';
 
-//import { activeUsersId } from './Reducers/activeUsersId';
 const Chat = ({ message }) => {
 	const { text, is_user_msg } = message;
-	return <span className={`Chat ${is_user_msg} ? "is-user-msg": ""}`}>{text}</span>;
+	return <span className={`Chat ${is_user_msg ? 'is-user-msg' : ''}`}>{text}</span>;
 };
+
 class Chats extends Component {
 	render() {
-		const { messages } = this.props;
 		return (
-			<div className="Chats">{messages.map((message) => <Chat message={message} key={message.number} />)}</div>
+			<div className="Chats">
+				{this.props.messages.map((message) => <Chat message={message} key={message.number} />)}
+			</div>
 		);
 	}
 }
--- skypey/src/MessageInput.js
+++ skypey/src/MessageInput.js
@@ -8,16 +8,18 @@ const handleChange = (e) => {
 	e.preventDefault();
 	store.dispatch(setTypingValue(e.target.value));
 };
-const handleSubmit = (e) => {
+const handleSubmit = (e, value, activeUser) => {
 	e.preventDefault();
-	const { typing, activeUsersId } = state;
-	store.dispatch(Send(typing, activeUsersId));
+	store.dispatch(Send(value, activeUser));
+	//console.log(value, activeUser);
 };
 const MessageInput = () => {
 	const value = store.getState().typing;
+	let activeUser = store.getState().activeUsersId;
 	return (
-		<form className="Message" onSubmit={handleSubmit}>
+		<form className="Message" onSubmit={(e) => handleSubmit(e, value, activeUser)}>
 			<input type="text" onChange={handleChange} value={value} placeholder="Write a message" />
+			<input type="submit" />
 		</form>
 	);
 };
